6f02233aafc39105432fa972fa78b285911853a7
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-09-11 12:52:06 +0200

    Listor och Arrayer
    Läxa För v.37

---
+++ Listor.och.Arrayer/Listor.och.Arrayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Listor.och.Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
