8f467d3f836b2441261a1bb7f3df8e92d692a5e8
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-09-25 15:06:30 +0200

    Startat igång läxan

---
+++ Läxa.oop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Läxa.oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
