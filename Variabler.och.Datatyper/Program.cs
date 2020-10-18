dd06c00135543018a625f556aabf49e955af6f4a
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-10-19 00:30:16 +0200

    Gjort klart variablar läxan
    Inte så svårt som jag trodde

--- Variabler.och.Datatyper/Program.cs
+++ Variabler.och.Datatyper/Program.cs
@@ -6,17 +6,32 @@ namespace Variabler.och.Datatyper
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Hello Niklas");
+
+            //Frågar om namn ålder och om du är vid liv 
             Console.WriteLine("Skriv Din Ålder");
             Console.WriteLine("Skriv Ditt Namn");
-            Console.WriteLine("Skriv om Du är vid liv"); 
+            Console.WriteLine("Skriv om Du är död eller levande"); 
+            //tar input från användaren 
             int age = Convert.ToInt32(Console.ReadLine());
             string namn = (Console.ReadLine());
+            string liv = (Console.ReadLine());
+
+            // Enkel if sats som skriver ut om du är död eller vid liv 
+            if (liv == "död")
+            {
+                Console.WriteLine("Du är död");
+            }
+            else if (liv == "levande")
+            {
+                Console.WriteLine("Du är Vid liv");
+            }
 
 
-            Console.WriteLine("Din ålder är : " + age );
+            // skriver ut input från användaren 
+            Console.WriteLine("Din ålder är  " + age );
             Console.WriteLine("Ditt namn är " + namn);
-
+            
 
 
         }
