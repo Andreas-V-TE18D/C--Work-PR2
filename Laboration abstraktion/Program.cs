c474296971818bc314dc57c5f51c43d20d608706
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-12-11 14:20:22 +0100

    börjat på labb abstrakt

---
+++ Laboration abstraktion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboration_abstraktion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
